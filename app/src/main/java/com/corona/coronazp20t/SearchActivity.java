42c91685231c4ea133e91e61bf8124f8a248eeba
Linas Pelenis <lpelenis@gmail.com>
2020-12-04 17:06:51 +0200

    Try add search possibility but does work app correctly, after login app stop working

--- app/src/main/java/com/corona/coronazp20t/SearchActivity.java
+++ app/src/main/java/com/corona/coronazp20t/SearchActivity.java
@@ -1,19 +1,38 @@
 package com.corona.coronazp20t;
 
-import android.app.ProgressDialog;
-import android.os.AsyncTask;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ProgressDialog;
+import android.app.SearchManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.SearchView;
 import android.widget.Toast;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Context;
+import android.content.Intent;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONArray;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.util.ArrayList;
+
 
 public class SearchActivity extends AppCompatActivity {
     public static final String COVID_API = "https://covid19-api.weedmark.systems/api/v1/stats";
+
+    private RecyclerView recyclerView;
+    private Adapter adapter;
+
+    private ArrayList<Corona> coronaList = new ArrayList<Corona>();
+
+    SearchView searchView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +43,57 @@ public class SearchActivity extends AppCompatActivity {
         asyncFetch.execute();
     }
 
-    private class AsyncFetch extends AsyncTask<String, String, JSONObject> {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // adds item to action bar
+        getMenuInflater().inflate(R.menu.search, menu);
+
+        // Get Search item from action bar and Get Search service
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchManager searchManager = (SearchManager) SearchActivity.this.getSystemService(Context.SEARCH_SERVICE);
+        if (searchItem != null) {
+            searchView = (SearchView) searchItem.getActionView();
+        }
+        if (searchView != null) {
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(SearchActivity.this.getComponentName()));
+            searchView.setIconified(false);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Every time when you press search button on keypad an Activity is recreated which in turn calls this function
+    @Override
+    protected void onNewIntent(Intent intent) {
+        // Get search query
+        super.onNewIntent(intent);
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            if (searchView != null) {
+                searchView.clearFocus();
+            }
+
+            // From all countries covid list creates specific list by searched country
+            ArrayList<Corona> coronaListByCountry = JSON.getCoronaListByCountry(coronaList, query);
+
+            if (coronaListByCountry.size() == 0) {
+                Toast.makeText(this, getResources().getString(R.string.search_no_results) + query, Toast.LENGTH_SHORT).show();
+            }
+
+            // Setup and Handover data to recyclerview
+            recyclerView = (RecyclerView) findViewById(R.id.corona_list);
+            adapter = new Adapter(SearchActivity.this, coronaListByCountry);
+            recyclerView.setAdapter(adapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(SearchActivity.this));
+        }
+    }
+
+    private class AsyncFetch extends AsyncTask<String, String, ArrayList<Corona>> {
         ProgressDialog pdLoading = new ProgressDialog(SearchActivity.this);
 
         @Override
@@ -34,14 +103,56 @@ public class SearchActivity extends AppCompatActivity {
             pdLoading.setMessage(getResources().getString(R.string.search_loading_data));
             pdLoading.setCancelable(false);
             pdLoading.show();
+            System.out.println("3333333333333333333333333333333333333333333333");
         }
 
         @Override
-        protected JSONObject doInBackground(String... params) {
+        protected ArrayList<Corona> doInBackground(String... params) {
             try {
                 JSONObject jsonObject = JSON.readJsonFromUrl(COVID_API);
-                return jsonObject;
 
+                int statusCode = 0;
+                try {
+                    statusCode = (Integer) jsonObject.get("statusCode");
+                } catch (JSONException e) {
+                    Toast.makeText(
+                            SearchActivity.this,
+                            getResources().getText(R.string.search_error_reading_data) + e.getMessage(),
+                            Toast.LENGTH_LONG
+                    ).show();
+                }
+                if (statusCode == HttpURLConnection.HTTP_OK) {
+                    System.out.println("5555555555555555555555555555555555555555555555");
+                    JSONArray jsonArray = null;
+                    coronaList = new ArrayList<Corona>();
+                    try {
+                        jsonArray = JSON.getJSONArray(jsonObject);
+                        coronaList = JSON.getList(jsonArray);
+                    } catch (JSONException e) {
+                        Toast.makeText(
+                                SearchActivity.this,
+                                getResources().getText(R.string.search_error_reading_data) + e.getMessage(),
+                                Toast.LENGTH_LONG
+                        ).show();
+                    }
+                    return coronaList;
+                } else { // something went wrong
+                    String message = null;
+                    try {
+                        message = (String) jsonObject.get("message");
+                    } catch (JSONException e) {
+                        Toast.makeText(
+                                SearchActivity.this,
+                                getResources().getText(R.string.search_error_reading_data) + e.getMessage(),
+                                Toast.LENGTH_LONG
+                        ).show();
+                    }
+                    Toast.makeText(
+                            SearchActivity.this,
+                            getResources().getText(R.string.search_error_reading_data) + message,
+                            Toast.LENGTH_LONG
+                    ).show();
+                }
             } catch (JSONException | IOException e1) {
                 Toast.makeText(
                         SearchActivity.this,
@@ -50,43 +161,17 @@ public class SearchActivity extends AppCompatActivity {
                 ).show();
                 return null;
             }
-        }
+            return null;
+        }// doInBackground
 
         @Override
-        protected void onPostExecute(JSONObject json) {
+        protected void onPostExecute(ArrayList<Corona> coronaList) {
             //this method will be running on UI thread
             pdLoading.dismiss();
 
-            int statusCode = 0;
-            try {
-                statusCode = (Integer) json.get("statusCode");
-            } catch (JSONException e) {
-                Toast.makeText(
-                        SearchActivity.this,
-                        getResources().getText(R.string.search_error_reading_data) + e.getMessage(),
-                        Toast.LENGTH_LONG
-                ).show();
+            if (coronaList != null) {
+                Toast.makeText(SearchActivity.this, getResources().getString(R.string.search_found_entries_from_api) + coronaList.size(), Toast.LENGTH_SHORT).show();
             }
-            if (statusCode == HttpURLConnection.HTTP_OK) {
-                System.err.println(json.toString());
-                Toast.makeText(SearchActivity.this, json.toString(), Toast.LENGTH_LONG).show();
-            } else { // something went wrong
-                String message = null;
-                try {
-                    message = (String) json.get("message");
-                } catch (JSONException e) {
-                    Toast.makeText(
-                            SearchActivity.this,
-                            getResources().getText(R.string.search_error_reading_data) + e.getMessage(),
-                            Toast.LENGTH_LONG
-                    ).show();
-                }
-                Toast.makeText(
-                        SearchActivity.this,
-                        getResources().getText(R.string.search_error_reading_data) + message,
-                        Toast.LENGTH_LONG
-                ).show();
-            }//else
         }//onPostExecute
     }//AsyncFetch class
 }
